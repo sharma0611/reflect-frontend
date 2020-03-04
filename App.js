bf35a21e7a1781268effff5108b217ca39436a9c
mattiaseyram <mattiaseyram@gmail.com>
2020-03-04 12:49:33 -0500

    switched useUser to reactn global state

--- App.js
+++ App.js
@@ -7,13 +7,15 @@ import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'Apollo/client'
 import { Provider as UnstatedProvider } from 'unstated'
 import * as Sentry from '@sentry/react-native'
+import { USER, initialUserState } from './App/Hooks/useUser'
 
 Sentry.init({
     dsn: 'https://578f3466d9ae487c9755bd9f1c7bb4c4@sentry.io/1878431'
 })
 // Set an initial global state directly:
 setGlobal({
-    name: ''
+    name: '',
+    [USER]: initialUserState
 })
 
 const App = () => {
--- App/Hooks/useUser.js
+++ App/Hooks/useUser.js
@@ -1,18 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useGlobal, useEffect } from 'reactn'
 import { auth } from '../Controllers/FirebaseController'
 import { hasProByUid } from '../Controllers/PurchasesController'
 
+export const USER = 'user'
+export const initialUserState = { loading: true, hasPro: false, uid: undefined }
 /**
  * returns user state
  * @param {Boolean} listen Specifies if data is refetched when the firebase currentUser changes
  * @returns {{ loading, hasPro, uid, refetch }}
  */
 export default function useUser({ listen = false, timeout = undefined }) {
-    const [{ loading, hasPro, uid }, setUser] = useState({
-        loading: true,
-        hasPro: false,
-        uid: undefined
-    })
+    const [{ loading, hasPro, uid }, setUser] = useGlobal(USER)
 
     const setUserTimeout = user => {
         setUser({ loading: true, hasPro, uid })
