7393c84e18fc5130a61df384ad53636bcb9384c0
Nasr <nasr.mohamed244@gmail.com>
2020-03-04 18:03:18 -0500

    env tracking for Sentry

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ import RootContainer from 'Containers/RootContainer'
 import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'Apollo/client'
 import { Provider as UnstatedProvider } from 'unstated'
+import AppConfig from 'Config/AppConfig'
 import * as Sentry from '@sentry/react-native'
 
 Sentry.init({
-    dsn: 'https://578f3466d9ae487c9755bd9f1c7bb4c4@sentry.io/1878431'
+    dsn: 'https://578f3466d9ae487c9755bd9f1c7bb4c4@sentry.io/1878431',
+    environment: AppConfig.isDev ? 'dev' : 'production'
 })
 // Set an initial global state directly:
 setGlobal({
