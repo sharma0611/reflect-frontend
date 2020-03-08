e8ce8c3643530eb5a0bf4df76966650e033d7390
sharma0611 <sharma.shivam0611@gmail.com>
2020-03-07 22:40:14 -0500

    working daily reflection set time

--- App/MellowContainers/EditDailyReminderScreen.js
+++ App/MellowContainers/EditDailyReminderScreen.js
@@ -6,16 +6,61 @@ import WaveBackground from 'MellowComponents/WaveBackground'
 import LeftChevron from 'MellowComponents/LeftChevron'
 import { withNavigation } from 'react-navigation'
 import DateTimePicker from '@react-native-community/datetimepicker'
+import Me from 'Apollo/queries/me'
+import updateUserReflectionTime from 'Apollo/mutations/updateUserReflectionTimeMutation'
+import { localToUtcTime, utcToLocalTime } from 'utils'
+import AppConfig from 'Config/AppConfig'
+import ErrorScreen from './ErrorScreen'
+import Card from 'MellowComponents/Card'
+import LoadingSpinner from 'Components/LoadingSpinner'
 
 const EditDailyReminderScreen = ({ navigation }) => {
-    const [time, setTime] = useState(new Date(1598051730000))
+    const [{ reflectionTimeHour, reflectionTimeMin, reflectionTime }, setTime] = useState({})
+    // const [time, setTime] = useState()
+
     const onChange = (event, selectedTime) => {
-        const currentTime = selectedTime || time
-        setTime(currentTime)
+        // let reflectionTime = new Date()
+        // if (reflectionTimeHour) {
+        //     reflectionTime.setHours(reflectionTimeHour)
+        //     reflectionTime.setMinutes(reflectionTimeMin)
+        // }
+        const hour = selectedTime.getHours()
+        const min = selectedTime.getMinutes()
+        setTime({
+            reflectionTimeHour: hour,
+            reflectionTimeMin: min,
+            reflectionTime: selectedTime
+        })
     }
 
-    const onSubmit = () => {
+    const onSubmit = async () => {
+        await submitTime()
         navigation.goBack()
+    }
+
+    const submitTime = async () => {
+        const { hours: utcHours, minutes: utcMinutes } = localToUtcTime(
+            reflectionTimeHour,
+            reflectionTimeMin
+        )
+        await updateUserReflectionTime(AppConfig.DEVICE_ID, utcHours, utcMinutes)
+        // Analytics.setDailyReflectionReminder()
+        // this.hideTimePicker()
+    }
+
+    const getLocalTimeFromData = data => {
+        const {
+            me: {
+                reflectionPush: { reflectionTimeHour: hour, reflectionTimeMin: min }
+            }
+        } = data
+        const { hours, minutes } = utcToLocalTime(hour, min)
+        return { hours, minutes }
+    }
+
+    const setReflectionTime = data => {
+        const { hours, minutes } = getLocalTimeFromData(data)
+        setTime({ reflectionTimeHour: hours, reflectionTimeMin: minutes })
     }
 
     return (
@@ -23,21 +68,41 @@ const EditDailyReminderScreen = ({ navigation }) => {
             <V p={4}>
                 <LeftChevron />
             </V>
-            <V p={4} pt={0}>
-                <T heading4 color="Gray1" pt={4}>
-                    Set your reminder time!
-                </T>
-                <DateTimePicker
-                    testID="dateTimePicker"
-                    value={time}
-                    mode={'time'}
-                    display="default"
-                    onChange={onChange}
-                />
-            </V>
-            <V ai="center" pt={2}>
-                <MainButton onPress={onSubmit} text={`Set time`} />
-            </V>
+            <Me
+                deviceId={AppConfig.DEVICE_ID}
+                onCompleted={data => setReflectionTime(data)}
+                // onError={error => setErrorState()}
+            >
+                {({ loading, error, data }) => {
+                    if (loading) return <LoadingSpinner />
+                    if (error) return <ErrorScreen {...{ error }} />
+                    let time = new Date()
+                    const { hours, minutes } = getLocalTimeFromData(data)
+                    time.setHours(hours)
+                    time.setMinutes(minutes)
+                    return (
+                        <>
+                            <V p={4} pt={0}>
+                                <T heading4 color="Gray1" pt={4}>
+                                    Set your reminder time!
+                                </T>
+                                <Card mt={4} alt bg="WhiteM">
+                                    <DateTimePicker
+                                        testID="dateTimePicker"
+                                        value={reflectionTime ? reflectionTime : time}
+                                        mode={'time'}
+                                        display="default"
+                                        onChange={onChange}
+                                    />
+                                </Card>
+                            </V>
+                            <V ai="center" pt={2}>
+                                <MainButton onPress={onSubmit} text={`Set time`} />
+                            </V>
+                        </>
+                    )
+                }}
+            </Me>
         </WaveBackground>
     )
 }
