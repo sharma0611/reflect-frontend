94626f8835a8d15d206c5a06356701fa4fdad81e
mattiaseyram <mattiaseyram@gmail.com>
2020-03-21 19:28:34 -0400

    Refactored useMoods

--- App/Firebase/helpers/index.js
+++ App/Firebase/helpers/index.js
@@ -1,11 +1,20 @@
 // @flow
 import firestore from '@react-native-firebase/firestore'
 import mapValues from 'lodash-es/mapValues'
+import moment from 'moment'
 
 export const dateToTimestamp = (d: any) => (d instanceof Date ? firestore.Timestamp.fromDate(d) : d)
 export const timestampToDate = (t: any) => (t instanceof firestore.Timestamp ? t.toDate() : t)
 export const nowTimestamp = (): firestore.Timestamp => firestore.Timestamp.now()
 export const nowDate = (): Date => new Date()
+export const startOfDay = (date: Date) =>
+    moment(date)
+        .startOf('day')
+        .toDate()
+export const endOfDay = (date: Date) =>
+    moment(date)
+        .endOf('day')
+        .toDate()
 
 export const mapDateValuesToTimestamp = (obj: {}): {} => mapValues(obj, dateToTimestamp)
 export const mapTimestampValuesToDate = (obj: {}): {} => mapValues(obj, timestampToDate)
--- App/Firebase/models/Entry.js
+++ App/Firebase/models/Entry.js
@@ -1,7 +1,11 @@
 // @flow
+import firestore from '@react-native-firebase/firestore'
 import Model from './Model'
+import Profile from './Profile'
+import { startOfDay, endOfDay } from '../helpers'
 
 const COLLECTION_NAME = 'entries'
+const DAILY_MOOD = 'dailyMood'
 
 type fields = {
     caption: string,
@@ -14,7 +18,32 @@ type fields = {
     uid: string
 }
 
-class EntryModel extends Model {}
+class EntryModel extends Model {
+    userQuery(): firestore.Query {
+        const uid = Profile.uid()
+        return this.collectionRef.where('uid', '==', uid)
+    }
+
+    moodsQuery(startDate?: Date, endDate?: Date) {
+        let query = this.userQuery().where('type', '==', DAILY_MOOD)
+        if (startDate) query = query.where('timestamp', '>=', startOfDay(startDate))
+        if (endDate) query = query.where('timestamp', '<', endOfDay(endDate))
+        return query
+    }
+
+    moods(startDate?: Date, endDate?: Date) {
+        return this.moodsQuery(startDate, endDate)
+    }
+
+    listenToMoods(
+        startDate?: Date,
+        endDate?: Date,
+        onData: (data: Array<{}>) => void,
+        onError: (error: Error) => void
+    ) {
+        return this.listenToQuery(this.moodsQuery(startDate, endDate), onData, onError)
+    }
+}
 
 const Entry = new EntryModel(COLLECTION_NAME)
 export default Entry
