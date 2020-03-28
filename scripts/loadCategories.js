a4fd7ad7baed1b067e8a54e9283366ca32703010
sharma0611 <sharma.shivam0611@gmail.com>
2020-03-27 20:52:29 -0400

    working batch delete

--- scripts/loadCategories.js
+++ scripts/loadCategories.js
@@ -78,18 +78,60 @@ const stringToIdList = str => {
     return str.split(',').map(id => id.trim())
 }
 
-const deleteCollection = async (db, collection) => {
-    let batch = db.batch()
-    await db
-        .collection(collection)
-        .listDocuments()
-        .then(val => {
-            val.map(val => {
-                batch.delete(val)
+// const deleteCollection = async (db, collection) => {
+//     let batch = db.batch()
+//     await db
+//         .collection(collection)
+//         .listDocuments()
+//         .then(val => {
+//             val.map(val => {
+//                 batch.delete(val)
+//             })
+//             batch.commit()
+//         })
+// }
+
+function deleteCollection(db, collectionPath, batchSize = 50) {
+    let collectionRef = db.collection(collectionPath)
+    let query = collectionRef.orderBy('__name__').limit(batchSize)
+
+    return new Promise((resolve, reject) => {
+        deleteQueryBatch(db, query, resolve, reject)
+    })
+}
+
+function deleteQueryBatch(db, query, resolve, reject) {
+    query
+        .get()
+        .then(snapshot => {
+            // When there are no documents left, we are done
+            if (snapshot.size === 0) {
+                return 0
+            }
+
+            // Delete documents in a batch
+            let batch = db.batch()
+            snapshot.docs.forEach(doc => {
+                batch.delete(doc.ref)
             })
 
-            batch.commit()
+            return batch.commit().then(() => {
+                return snapshot.size
+            })
         })
+        .then(numDeleted => {
+            if (numDeleted === 0) {
+                resolve()
+                return
+            }
+
+            // Recurse on the next process tick, to avoid
+            // exploding the stack.
+            process.nextTick(() => {
+                deleteQueryBatch(db, query, resolve, reject)
+            })
+        })
+        .catch(reject)
 }
 
 const processCategoryJson = raw => {
